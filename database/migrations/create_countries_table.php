931a63cb1439b68454d5b18cb99b5ac39cd3607f
AndreasZiagl <a.ziegelwanger@gmail.com>
2022-03-30 10:19:47 +0200

    Upload Files

---
+++ database/migrations/create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->string('code', 2)->index()->primary();
+            $table->string('en');
+            $table->string('de');
+            $table->string('es');
+            $table->string('fr');
+            $table->string('it');
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
